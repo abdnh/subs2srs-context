3215c25cf4d986449f2610186bf875c6b8878fd8
Abdo <abd.nh25@gmail.com>
2022-04-19 19:43:31 +0300

    Only trigger search when a toggle is clicked
    This is to prevent review slowdowns on large collections when the add-on's
    functionality is not needed.

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,4 @@
+import json
 from typing import Any, Tuple
 import re
 
@@ -7,15 +8,19 @@ from anki.template import TemplateRenderContext
 from anki.collection import SearchNode
 from aqt import mw
 from aqt.sound import play
+from aqt.browser.previewer import Previewer
+from aqt.clayout import CardLayout
 
 from . import consts
 
-PLAY_BUTTON = """<a class="replay-button soundLink" href=# onclick="pycmd('{cmd}:{filename}'); return false;">
+PLAY_BUTTON = """<a class="replay-button soundLink" href=# onclick="pycmd('{cmd}:play:{filename}'); return false;">
     <svg class="playImage" viewBox="0 0 64 64" version="1.1" style="{transform}">
         <circle cx="32" cy="32" r="29" />
         <path d="M56.502,32.301l-37.502,20.101l0.329,-40.804l37.173,20.703Z" />
     </svg>
 </a>"""
+
+TOGGLE_CONTEXT_BUTTON = """<button id="subs2srs-context-toggle" onclick="pycmd('{cmd}:show:{notetype}:{ep}:{seq}'); return false;" style="display: block; margin: 5px auto;">{label}</button>"""
 
 SOUND_REF_RE = re.compile(r"\[sound:(.*?)\]")
 NOTES_FIELD_RE = re.compile(r"(?P<ep>\d+)_(?P<seq>\d+)")
@@ -36,6 +41,24 @@ def add_filter(
     notetype = mw.col.models.get(note.mid)["name"]
     ep = match.group("ep")
     seq = int(match.group("seq"))
+
+    button_text = TOGGLE_CONTEXT_BUTTON.format(
+        cmd=consts.FILTER_NAME,
+        label=consts.ADDON_NAME,
+        notetype=notetype,
+        ep=ep,
+        seq=seq,
+    )
+    return field_text + button_text
+
+
+def toggle_context_buttons(context: Any, notetype: str, ep: int, seq: int):
+    if isinstance(context, Previewer):
+        web = context._web
+    elif isinstance(context, CardLayout):
+        web = context.preview_web
+    else:
+        web = context.web
 
     def add_sound_button(side: str, seq: str) -> str:
         nonlocal notetype, ep
@@ -65,7 +88,14 @@ def add_filter(
     next_button_text = add_sound_button("next", seq + 1)
     prev_button_text = add_sound_button("prev", seq - 1)
     buttons_text = f"<div>{prev_button_text}{next_button_text}</div>"
-    return field_text + buttons_text
+    js = f"""
+var subs2srsContextToggle = document.getElementById('subs2srs-context-toggle');
+if(!subs2srsContextToggle.dataset.shown) {{
+    subs2srsContextToggle.insertAdjacentHTML('afterend', {json.dumps(buttons_text)});
+    subs2srsContextToggle.dataset.shown = true;
+}}
+"""
+    web.eval(js)
 
 
 def handle_play_message(handled: Tuple[bool, Any], message: str, context: Any):
@@ -73,8 +103,15 @@ def handle_play_message(handled: Tuple[bool, Any], message: str, context: Any):
     cmd = parts[0]
     if cmd != consts.FILTER_NAME:
         return handled
-    filename = parts[1]
-    play(filename)
+    subcmd = parts[1]
+    if subcmd == "play":
+        filename = parts[2]
+        play(filename)
+    elif subcmd == "show":
+        notetype = parts[2]
+        ep = int(parts[3])
+        seq = int(parts[4])
+        toggle_context_buttons(context, notetype, ep, seq)
     return (True, None)
 
 
--- src/consts.py
+++ src/consts.py
@@ -1,2 +1,2 @@
-ADDON_NAME = "subs2srs Context"
+ADDON_NAME = "subs2srs context"
 FILTER_NAME = "subs2srs-context"
