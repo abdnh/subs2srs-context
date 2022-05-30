05ce90617aef005889801358468fe7f4565faef1
Abdo <abd.nh25@gmail.com>
2022-05-30 09:22:25 +0300

    Accept caller-provided collection
    I mainly intend to use this in my copyaround add-on to fetch contents
    from another profile.

--- src/subs2srs_context.py
+++ src/subs2srs_context.py
@@ -1,6 +1,7 @@
 import re
-from typing import List
+from typing import List, Optional
 
+from anki.collection import Collection
 from anki.errors import NotFoundError
 from anki.notes import NoteId
 from aqt import mw
@@ -18,9 +19,10 @@ class Subs2srsContext:
 
     SOUND_REF_RE = re.compile(r"\[sound:(.*?)\]")
 
-    def get_audio_filename(self, nid: NoteId) -> str:
+    def get_audio_filename(self, nid: NoteId, col: Optional[Collection] = None) -> str:
+        col = col or mw.col
         try:
-            note = mw.col.get_note(nid)
+            note = col.get_note(nid)
         except NotFoundError:
             return ""
         if "Audio" not in note:
@@ -31,10 +33,16 @@ class Subs2srsContext:
             return match.group(1)
         return ""
 
-    def get_audio_button(self, nid: NoteId, position: str, flip: bool = False) -> str:
+    def get_audio_button(
+        self,
+        nid: NoteId,
+        position: str,
+        flip: bool = False,
+        col: Optional[Collection] = None,
+    ) -> str:
         button_text = ""
         neighbor_nid = (nid + 1) if position == "next" else (nid - 1)
-        filename = self.get_audio_filename(NoteId(neighbor_nid))
+        filename = self.get_audio_filename(NoteId(neighbor_nid), col=col)
         if filename:
             button_text = self.PLAY_BUTTON.format(
                 cmd=consts.FILTER_NAME,
@@ -43,18 +51,23 @@ class Subs2srsContext:
             )
         return button_text
 
-    def get_audio_buttons(self, nid: NoteId, flip: bool = True) -> List[str]:
-        prev_button = self.get_audio_button(nid, "prev", flip=flip)
-        next_button = self.get_audio_button(nid, "next")
+    def get_audio_buttons(
+        self, nid: NoteId, flip: bool = True, col: Optional[Collection] = None
+    ) -> List[str]:
+        prev_button = self.get_audio_button(nid, "prev", flip=flip, col=col)
+        next_button = self.get_audio_button(nid, "next", col=col)
         return [prev_button, next_button]
 
-    def get_expressions(self, nid: NoteId) -> List[str]:
+    def get_expressions(
+        self, nid: NoteId, col: Optional[Collection] = None
+    ) -> List[str]:
         """Get the contents of the Expression field for the previous and next subs2srs notes."""
+        col = col or mw.col
         contents = []
         nids = [nid - 1, nid + 1]
         for n in nids:
             try:
-                note = mw.col.get_note(NoteId(n))
+                note = col.get_note(NoteId(n))
             except NotFoundError:
                 contents.append("")
                 continue
